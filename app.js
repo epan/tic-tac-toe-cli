3089b9ec04ea5fabdebb97d00e54e5ef0088fe73
Eric Pan <epan@users.noreply.github.com>
2017-06-19 14:52:15 -0700

    (feat) get user input

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // PLAN
 // Draw to command line
+//
 
-console.log('hello');
+const prompt = require('prompt');
+prompt.start();
+
+function askMove() {
+  prompt.get('P1move', (err, result) => {
+    console.log(result.P1move);
+  });
+}
+
+askMove();
